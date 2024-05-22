8577f6353af448454e0cf1c5f9063f7aecd1ef42
username57564 <138431154+username57564@users.noreply.github.com>
2024-05-22 13:19:34 +0900

    first commit

--- raman_peaks.py
+++ raman_peaks.py
@@ -1,7 +1,11 @@
+#@title RamanPeaks_v2
 import yaml
 
 class RamanPeaks:
     def __init__(self, yaml_file):
+        if yaml_file is None:
+            yaml_file = os.path.join(os.path.dirname(__file__), 'data', 'raman_peaks.yaml')
+        
         with open(yaml_file, 'r') as file:
             data = yaml.safe_load(file)['raman_peaks']
         
@@ -9,42 +13,44 @@ class RamanPeaks:
         self.main_peaks = {}
         self.sub_peaks = {}
 
-        for substance, peaks in data.items():
-            if substance == 'VO2':
-                for sub_type, sub_peaks in peaks.items():
-                    key = f"{substance} ({sub_type})"
-                    self.data[key] = set(sub_peaks['all'])
-                    self.main_peaks[key] = set(sub_peaks['main'])
-                    self.sub_peaks[key] = self.data[key] - self.main_peaks[key]
-                # Default to VO2 (M1)
-                self.data[substance] = self.data[f"{substance} (M1)"]
-                self.main_peaks[substance] = self.main_peaks[f"{substance} (M1)"]
-                self.sub_peaks[substance] = self.sub_peaks[f"{substance} (M1)"]
-            else:
-                self.data[substance] = set(peaks['all'])
-                self.main_peaks[substance] = set(peaks['main'])
-                self.sub_peaks[substance] = self.data[substance] - self.main_peaks[substance]
+        default_substance = list(data.keys())[0]
 
-    def add_peaks(self, substance_name: str, raman_peaks: set, main_peaks=()):
-        if substance_name in self.data:
-            print(f"{substance_name} already exists. Overwriting the data.")
-        self.data[substance_name] = set(raman_peaks)
-        self.main_peaks[substance_name] = set(main_peaks)
-        self.sub_peaks[substance_name] = set(raman_peaks) - set(main_peaks)
+        for substance, details in data.items():
+            # TO DO: details가 all, main, 다른 phase 정보(all, main)로 구성된 경우
+            if isinstance(details[list(details.keys())[0]], dict):
+                # 첫번째 subtype을 대푯값으로 저장
+                general = details[list(details.keys())[0]]
+                self.add_peaks(substance, set(general['all']), main_peaks=set(general['main']))
+                for sub_type, peaks in details.items():
+                    key = f"{substance} ({sub_type})"
+                    self.add_peaks(substance, set(peaks['all']), main_peaks=set(peaks['main']), sub_type=sub_type)
+            else:
+                self.add_peaks(substance, set(details['all']), main_peaks=set(details['main']))
+
+    def add_peaks(self, substance_name: str, raman_peaks: set, main_peaks=(), sub_type=None):
+        key = f"{substance_name} ({sub_type})" if sub_type else substance_name
+        if key in self.data:
+            print(f"{key} already exists. Overwriting the data.")
+        self.data[key] = set(raman_peaks)
+        self.main_peaks[key] = set(main_peaks)
+        self.sub_peaks[key] = set(raman_peaks) - set(main_peaks)
 
     def get_peaks(self, substance_name: str, which: str = "all") -> set:
         if which == "all":
-            return self.data.get(substance_name)
+            return self.data.get(substance_name, None)
         elif which == "main":
-            return self.main_peaks.get(substance_name)
+            return self.main_peaks.get(substance_name, None)
         elif which == "sub":
-            return self.sub_peaks.get(substance_name)
+            return self.sub_peaks.get(substance_name, None)
         else:
             print("Invalid input for 'which' parameter. Choose from 'all', 'main', or 'sub'.")
             return None
 
     def list_substances(self) -> list:
         return list(self.data.keys())
+    
+    def get_default_substance(self) -> str:
+        return list(self.data.keys())[0]
 
 if __name__ == "__main__":
     # YAML 파일에서 데이터를 로드하여 RamanPeaks 인스턴스 생성
